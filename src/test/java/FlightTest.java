e6a68619d38a3a86b46645a5726ee3cb420e55a8
naz_liton <nazrulliton@icloud.com>
2020-01-10 12:35:55 +0000

    flighttest created

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,3 +1,12 @@
+import org.junit.Before;
+
 public class FlightTest {
+    Flight flight;
+
+    @Before
+    public void before(){
+        flight = new Flight("EZJT123", "EDI", "GLA", "13:00");
+    }
+    
 
 }
